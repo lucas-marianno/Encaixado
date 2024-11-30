d68b40b59ca0115a16d66ffbf1bc788c34d761e2
Lucas Marianno <lucas.marianno94@gmail.com>
2024-11-30 13:05:45 -0300

    added trigger for backspace hold down keystroke

--- lib/presentation/widgets/word_field.dart
+++ lib/presentation/widgets/word_field.dart
@@ -15,34 +15,27 @@ class WordField extends StatelessWidget {
     return KeyboardListener(
       focusNode: focusNode,
       onKeyEvent: (value) {
-        if (value is! KeyDownEvent) return;
+        if (value is KeyUpEvent) return;
+
         final keyLabel = value.logicalKey.keyLabel.toLowerCase();
 
+        if (keyLabel == 'backspace') controller.deleteLastLetter();
+        if (value is! KeyDownEvent) return;
+        if (keyLabel == 'enter') onSubmitted();
         if (controller.box.availableLetters.contains(keyLabel)) {
           controller.onAcceptDrag(keyLabel);
         }
-        if (keyLabel == 'backspace') {
-          controller.deleteLastLetter();
-        }
-        if (keyLabel == 'enter') {
-          onSubmitted();
-        }
       },
-      child: GestureDetector(
-        onTap: () {
-          print('tap');
-        },
-        child: Container(
-          color: Colors.transparent,
-          width: controller.boxSize * 1.3,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(controller.currentWord.toUpperCase()),
-              const Divider(height: 10, indent: 10, endIndent: 10),
-              Text(controller.currentSolution.join('  ->  ').toUpperCase()),
-            ],
-          ),
+      child: Container(
+        color: Colors.transparent,
+        width: controller.boxSize * 1.3,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(controller.currentWord.toUpperCase()),
+            const Divider(height: 10, indent: 10, endIndent: 10),
+            Text(controller.currentSolution.join('  ->  ').toUpperCase()),
+          ],
         ),
       ),
     );
